e60fc8919f62e1b41d3ffe88d75e8f1ae203e504
JariRo <jarirosenstrom@gmail.com>
2016-07-13 23:10:37 +0200

    Upload

---
+++ JariJuha/givename.py
@@ -0,0 +1,8 @@
+import random
+
+def name_giver():
+    mylist=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
+    "w","x","y","z"]
+    print (random.choice(mylist))
+
+name_giver()
